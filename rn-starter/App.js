2f6255afc8ad33ae4aaaa97fc498bd5fdb810f18
Spencer White <swhit210@ford.com>
2020-06-11 11:10:32 -0400

    added mock screens

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,20 +1,26 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-import ComponentsScreeen from "./src/screens/ComponentsScreen";
-import ListScreen from "./src/screens/ListScreen"
+import ListScreen from "./src/screens/ListScreen";
+import ConsentScreen from "./src/screens/ConsentScreen";
+import PrepareDeviceScreen from "./src/screens/PrepareDeviceScreen";
+import SurveyScreen from "./src/screens/SurveyScreen";
+import WellnessScoreScreen from "./src/screens/WellnessScoreScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreeen,
-    List: ListScreen
+    List: ListScreen,
+    Consent: ConsentScreen,
+    PrepareDevice: PrepareDeviceScreen,
+    Survey: SurveyScreen,
+    WellnessScore: WellnessScoreScreen,
   },
   {
-    initialRouteName: "List",
+    initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "App"
-    }
+      title: "App",
+    },
   }
 );
 
